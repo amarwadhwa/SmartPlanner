359575098bb76b3084a474c63ad6259df855874c
amarwadhwa <36704598+amarwadhwa@users.noreply.github.com>
2018-03-06 15:33:42 +0500

    Enageges Changes

--- application/views/Engages/PermanentEngages.php
+++ application/views/Engages/PermanentEngages.php
@@ -17,6 +17,191 @@
 <body>
 	
 	
-
+	<div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Engages</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+        
+<div id="no-more-tables">
+            <table class="col-sm-12 table-bordered table-striped table-condensed cf">
+        		<thead class="cf">
+        			<tr>
+						<th>      </th>
+        				<th>Monday</th>
+        				<th>Tuesday</th>
+        				<th>Wednesday</th>
+        				<th>Thursday</th>
+        				<th>Friday</th>
+        				<th>Saturday</th>
+        				
+        			</tr>
+        		</thead>
+        		<tbody>
+        			<tr>
+						<td data-title="      ">09:00 |1| 09:30</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">08:35 |2| 09:25
+								<div class="btn-group">
+                    <button type="button" class="btn btn-default"><i class="fa fa-check-circle fa-lg text-success"></i> Test</button>
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                      <span class="caret"></span>
+                      <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="#">Test &amp; Deploy</a></li>
+                      <li><a href="#">Deploy</a></li>
+                      <li><a href="#">Build</a></li>
+                      <li class="divider"></li>
+                      <li><a href="#">Disable</a></li>
+                    </ul>
+                  </div>
+                </td>
+</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">09:30 |3| 10:20</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">10:35 |4| 11:25</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">11:30 |5| 12:20</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">12:20 |6| 13:10</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">13:10 |7| 14:00</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">14:00 |8| 14:50</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">15:00 |9| 15:50</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">15:55 |10| 16:45	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        			<tr>
+						<td data-title="      ">16:50 |11| 17:40	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+					<tr>
+						<td data-title="      ">17:40 |12| 18:30	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+					<tr>
+						<td data-title="      ">18:55 |13| 19:40	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+					<tr>
+						<td data-title="      ">19:40 |14| 20:25	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+					<tr>
+						<td data-title="      ">20:30 |15| 21:15	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+					<tr>
+						<td data-title="      ">21:15 |16| 22:00	</td>
+        				<td data-title="Monday">Lesson</td>
+        				<td data-title="Tuesday">Lesson</td>
+        				<td data-title="Wednesday">Lesson</td>
+        				<td data-title="Thursday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        				<td data-title="Friday">Lesson</td>
+        			</tr>
+        		</tbody>
+        	</table>
+        </div>
+</div>
 </body>
 </html>
