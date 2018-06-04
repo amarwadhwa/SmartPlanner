bf34ecdef0ebdd26b6b06166a9b73c4596e41f97
amarwadhwa <36704598+amarwadhwa@users.noreply.github.com>
2018-06-05 03:40:23 +0500

    Dashboard Work

--- application/views/User/Dashboard/home.php
+++ application/views/User/Dashboard/home.php
@@ -79,10 +79,32 @@
                   </a>
                </div>
             </div>
+            <div class="col-lg-3 col-md-6">
+               <div class="panel panel-green">
+                  <div class="panel-heading">
+                     <div class="row">
+                        <div class="col-xs-3">
+                           <i class="fa fa-tasks fa-5x"></i>
+                        </div>
+                        <div class="col-xs-9 text-right">
+                           <div class="huge">12</div>
+                           <div>Scheduled Meeting</div>
+                        </div>
+                     </div>
+                  </div>
+                  <a href="#">
+                     <div class="panel-footer">
+                        <span class="pull-left">View Details</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                     </div>
+                  </a>
+               </div>
+            </div>
          </div>
          <!-- /.row -->
          <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-12">
                <!-- /.panel -->
                <div class="panel panel-default">
                   <div class="panel-heading">
@@ -110,15 +132,15 @@
                   <!-- /.panel-heading -->
                   <div class="panel-body">
                      <div class="row">
-                        <div class="col-lg-4">
+                        <div class="col-lg-12">
                            <div class="table-responsive">
                               <table class="table table-bordered table-hover table-striped">
                                  <thead>
                                     <tr>
-                                       <th>#</th>
-                                       <th>Date</th>
-                                       <th>Time</th>
-                                       <th>Amount</th>
+                                       <th>Meeting no.</th>
+                                       <th>Meeting Agenda</th>
+                                       <th>Timing</th>
+                                       <th>Committies</th>
                                     </tr>
                                  </thead>
                                  <tbody>
@@ -140,28 +162,6 @@
                <!-- /.panel -->
                <!-- /.panel -->
             </div>
-            <!-- /.col-lg-8 -->
-            <div class="col-lg-4">
-               <div class="panel panel-default">
-                  <div class="panel-heading">
-                     <i class="fa fa-bell fa-fw"></i> Notifications Panel
-                  </div>
-                  <!-- /.panel-heading -->
-                  <div class="panel-body">
-                     <a href="#" class="btn btn-default btn-block">View All Alerts</a>
-                  </div>
-                  <!-- /.panel-body -->
-               </div>
-               <!-- /.panel -->
-               <div class="panel panel-default">
-                  <div class="panel-heading">
-                     <i class="fa fa-bar-chart-o fa-fw"></i> Recent Meetings
-                  </div>
-                  <div class="panel-body">
-                     <div id="morris-donut-chart"></div>
-                     <a href="#" class="btn btn-default btn-block">View Details</a>
-                  </div>
-                  <!-- /.panel-body -->
                </div>
                <div class="panel-footer">
                   <div class="input-group">
