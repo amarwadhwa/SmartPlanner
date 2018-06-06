fd887e5f20689098decc2a21be330342e6facb57
Aakash <rockiing.aakash@gmail.com>
2018-06-07 00:42:16 +0500

    Revert "Tried alot but failed :P"
    This reverts commit 2109d88d81e843c6d5d50ba0b8741ad21729e691.

--- application/views/User/InitiateMeeting/initiatemeetingPage2.php
+++ application/views/User/InitiateMeeting/initiatemeetingPage2.php
@@ -20,28 +20,6 @@ function submitClick(){
   return true;
 }
 </script>
-
-
-
-<script type="text/javascript">
-    $(function () {
-        $('#datetimepicker6').datetimepicker();
-        $('#datetimepicker7').datetimepicker({
-            useCurrent: false //Important! See issue #1075
-        });
-        $("#datetimepicker6").on("dp.change", function (e) {
-            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-        });
-        $("#datetimepicker7").on("dp.change", function (e) {
-            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-        });
-    });
-</script>
-
-
-
-
-
 </head>
    <body>
       <div id="page-wrapper">
@@ -66,33 +44,6 @@ function submitClick(){
                               <article>
                                  <div class="demo">
                                     <h2>Date and Time </h2>
-<!--my nee-->
-<div class="container">
-    <div class='col-md-5'>
-        <div class="form-group">
-            <div class='input-group date' id='datetimepicker6'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
-    <div class='col-md-5'>
-        <div class="form-group">
-            <div class='input-group date' id='datetimepicker7'>
-                <input type='text' class="form-control" />
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
                                     <p id="datepairExample">
                                        <input type="text" class="date start" placeholder="Date"  name="start_date" required />
                                        <input type="text" class="time start" placeholder="Time" name="start_time" required /> to
@@ -127,7 +78,13 @@ function submitClick(){
                               <p><span id="txtHint"></span></p>  
                                </div>              
                          </div>
-                       <br>
+                        <div class="col-lg-12">
+                           
+                           <br>
+                           <div class="progress" class="col-lg-9">
+                              <div class="progress-bar" style="width:70%">70%</div>
+                           </div>
+                        </div>
                         <div>
                            <div class="row">
                               <div class="col-lg-12">
