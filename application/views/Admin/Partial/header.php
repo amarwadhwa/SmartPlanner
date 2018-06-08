b2033f1fe6b2dc28ce85dc68ef8dbfa41cfb2d9f
amarwadhwa <36704598+amarwadhwa@users.noreply.github.com>
2018-06-08 18:54:15 +0500

    Header set + New page
    New page Add Engages added

--- application/views/Admin/Partial/header.php
+++ application/views/Admin/Partial/header.php
@@ -31,73 +31,12 @@
          <!-- /.navbar-header -->
          <ul class="nav navbar-top-links navbar-right">
             <li class="dropdown">
-               <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-               <i class="fa fa-envelope fa-fw"></i> <i class="fa fa-caret-down"></i>
-               </a>
-               <ul class="dropdown-menu dropdown-messages">
-                  <li>
-                     <a href="#">
-                        
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a href="#">
-                       
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a href="#">
-                       
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a class="text-center" href="#">
-                     <strong>Read All Messages</strong>
-                     <i class="fa fa-angle-right"></i>
-                     </a>
-                  </li>
-               </ul>
+               
+               
                <!-- /.dropdown-messages -->
             </li>
             <!-- /.dropdown -->
-            <li class="dropdown">
-               <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-               <i class="fa fa-tasks fa-fw"></i> <i class="fa fa-caret-down"></i>
-               </a>
-               <ul class="dropdown-menu dropdown-tasks">
-                  <li>
-                     <a href="#">
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a href="#">
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a href="#">
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a href="#">
-                        
-                     </a>
-                  </li>
-                  <li class="divider"></li>
-                  <li>
-                     <a class="text-center" href="#">
-                     <strong>See All Tasks</strong>
-                     <i class="fa fa-angle-right"></i>
-                     </a>
-                  </li>
-               </ul>
-               <!-- /.dropdown-tasks -->
-            </li>
+            
             <!-- /.dropdown -->
             <li class="dropdown">
                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
@@ -137,17 +76,7 @@
          <div class="navbar-default sidebar" role="navigation">
             <div class="sidebar-nav navbar-collapse">
                <ul class="nav" id="side-menu">
-                  <li class="sidebar-search">
-                     <div class="input-group custom-search-form">
-                        <input type="text" class="form-control" placeholder="Search...">
-                        <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">
-                        <i class="fa fa-search"></i>
-                        </button>
-                        </span>
-                     </div>
-                     <!-- /input-group -->
-                  </li>
+                  
                   <li>
                      <a href="<?php echo base_url('Admin/index')?>"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
                   </li>
@@ -157,6 +86,10 @@
                   <li>
                      <a href="<?php echo base_url('AddCommitties/index')?>"><i class="fa fa-table fa-fw"></i> Committies</a>
                   </li>
+                  <li>
+                     <a href="<?php echo base_url('AddEngages/index')?>"><i class="fa fa-table fa-fw"></i> Add Engages</a>
+                  </li>
+                  
                   <li>
                      <a href="<?php echo base_url('AdminSettings/index')?>"><i class="fa fa-edit fa-fw"></i> Settings</a>
                   </li>
