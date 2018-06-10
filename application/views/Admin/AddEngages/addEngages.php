35896f8e8866a10dce583a3794a46f42ebe265b2
Karan Sachrani <karan.sachrani@gmail.com>
2018-06-10 07:47:48 +0500

    EngagesFileUploading

--- application/views/Admin/AddEngages/addEngages.php
+++ application/views/Admin/AddEngages/addEngages.php
@@ -16,7 +16,26 @@
                   
                   <div class="panel-body">
                      <div class="row">
-                        
+                        <form enctype="multipart/form-data" action="upload.php" method="POST">
+                        <p>Upload XML file Only</p>
+                        <input type="file" name="uploaded_file" ></input><br />
+                        <input type="submit" value="Upload"></input>
+                     </form>
+
+                     <?PHP
+                   if(!empty($_FILES['uploaded_file']))
+                  {
+                  $path = "SmartPlanner/EngagesByWaseem/";
+                  $path = $path . basename( $_FILES['uploaded_file']['name']);
+                  if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+                   echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+                  " has been uploaded";
+                  } else{
+                   echo "There was an error uploading the file, please try again!";
+                     }
+                  }
+                  ?>
+
                      <!-- /.col-lg-6 (nested) -->
                   </div>
                   <!-- /.col-lg-6 (nested) -->
